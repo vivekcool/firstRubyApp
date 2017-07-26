d7efb2f048dd0f6fbce71cefea324bb3684cad8a
VIVEK SHARMA <vsviveksharma999@gmail.com>
2017-07-26 09:22:12 +0530

    Authentication completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	before_action :require_login, only: [:new , :create , :update, :edit]
 	  include ArticlesHelper
 	def index
   @articles = Article.all
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ resources :articles do
   resources :comments
 end
   resources :tags
+    resources :authors
+    resources :author_sessions, only: [ :new, :create, :destroy ]
+
+get 'login'  => 'author_sessions#new'
+get 'logout' => 'author_sessions#destroy'
+
   root to: 'articles#index'
 end
